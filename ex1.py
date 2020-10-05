699e63a9de218dbb3381fcd994e6a3d35cba8ecd
neshprot <71090726+neshprot@users.noreply.github.com>
2020-10-05 23:30:08 +0300

    With comments

--- ex1.py
+++ ex1.py
@@ -5,12 +5,14 @@ import math
 import numpy
 import matplotlib.pyplot as mpp
 
+#импорт библиотек
+
 # Эта программа рисует график функции, заданной выражением ниже
 
 if __name__=='__main__':
-    arguments = numpy.r_[0:200:0.1]
-    mpp.plot(
+    arguments = numpy.r_[0:200:0.1] #Значения по оси 0X с шагом 0.1
+    mpp.plot( #подсчёт графика
         arguments,
-        [math.sin(a) * math.sin(a/20.0) for a in arguments]
+        [math.sin(a) * math.sin(a/20.0) for a in arguments] #заданная функция
     )
-    mpp.show()
+    mpp.show() #вывод графика
