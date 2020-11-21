3172b09da03cc28fe800bec6062ec20f141d965c
neshprot <nik.demid0v@mail.ru>
2020-11-21 17:11:21 +0300

    9.50/10 in pylint with disabled missing docstrings

---
+++ egcd.py
@@ -0,0 +1,26 @@
+import math
+
+
+def e_gcd(first_num, sec_num):
+    if sec_num == 0:
+        return first_num
+    kof2 = 1
+    kof1 = 0
+    gcd = e_gcd(sec_num, first_num % sec_num)
+    buf = kof2
+    kof2 = kof1 - (math.ceil(first_num / sec_num) - 1) * kof2
+    kof1 = buf
+    return kof1, kof2, gcd
+
+
+NUM1 = 1404
+NUM2 = 15912
+if NUM1 < NUM2:
+    NUM1, NUM2 = NUM2, NUM1
+if NUM2 == 0:
+    if NUM1 == 0:
+        print(NUM1, "*", 0, "+", NUM2, "*", 0, "=", 0)
+    else:
+        print(NUM1, "*", 1, "+", NUM2, "*", 0, "=", 0)
+else:
+    print(NUM1, "*", e_gcd(NUM1, NUM2)[0], "+", NUM2, "*", e_gcd(NUM1, NUM2)[1], "=", e_gcd(NUM1, NUM2)[2][2])
