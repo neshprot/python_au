c7b2bc0dd9605a2a3b7c1811e0b6fdbea783f9fe
neshprot <nik.demid0v@mail.ru>
2020-11-21 19:19:21 +0300

    9.55/10 in pylint with disabled missing docstrings

--- the_simplest_prime_num.py
+++ the_simplest_prime_num.py
@@ -3,11 +3,11 @@ import math
 
 def prime(value):
     if value % 2 == 0 | value % 10 == 0:
-        return "FALSE"
+        return "False"
     for i in range(3, math.ceil(math.sqrt(value)) - 1, 2):
         if value % i == 0:
-            return "FALSE"
-    return "TRUE"
+            return "False"
+    return "True"
 
 
 print(prime(8731))
