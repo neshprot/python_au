a699eebfbed7397c4bd571f832e702262b535d30
neshprot <nik.demid0v@mail.ru>
2020-11-24 01:37:09 +0300

    new version with bug fix

--- the_simplest_prime_num.py
+++ the_simplest_prime_num.py
@@ -2,12 +2,15 @@ import math
 
 
 def prime(value):
-    if value % 2 == 0 | (value > 10 & value % 10 == 0):
-        return "False"
+    if value % 2 == 0 or value % 10 == 0 or value % 3 == 0:
+        return False
     for i in range(3, math.ceil(math.sqrt(value)) - 1, 2):
         if value % i == 0:
-            return "False"
-    return "True"
+            return False
+    return True
 
 
-print(prime(8731))
+if prime(int(input())):
+    print("Число простое")
+else:
+    print("Число составное")
