3f942e3ee9b6bbf8c4a0155e342966f1642c468b
neshprot <71090726+neshprot@users.noreply.github.com>
2021-03-16 21:37:51 +0300

    Update paralleling.py
    Corrected code

--- paralleling.py
+++ paralleling.py
@@ -2,12 +2,12 @@ import random
 import multiprocessing
 import time
 
-number = 30000
+num = 10
 
 
 def count_pi(x):
     amount_circle = 0
-    for i in range(number):
+    for i in range(100000):
         x = random.uniform(0, 1)
         y = random.uniform(0, 1)
         if x * x + y * y <= 1:
@@ -15,12 +15,13 @@ def count_pi(x):
     return amount_circle
 
 
+def test_all(pool):
+    l = pool.map(count_pi, [0]*num)
+    return l
+
+
 if __name__ == '__main__':
     pool = multiprocessing.Pool()
     t0 = time.time()
-    list_circle = pool.map(count_pi, range(2))
-    S = 0
-    for i in range(2):
-      S += list_circle[i]
-    print(4*S/2/number)
+    print(4*sum(test_all(pool))/100000/num)
     print("Time spent:", time.time() - t0)
