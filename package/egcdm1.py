9486dc3785c37fc723ab94218420896696353d6a
neshprot <nik.demid0v@mail.ru>
2021-04-13 00:26:33 +0300

    Module

--- package/egcdm1.py
+++ package/egcdm1.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import egcdm2
 from package.egcdm2 import e_gcd
 def m1f1(NUM1, NUM2):
@@ -14,3 +15,21 @@ def m1f1(NUM1, NUM2):
         else:
             S = str(NUM1) + ' * ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[0]) + ' + ' + str(NUM2) + ' * ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[1]) + ' = ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[2][2])
     return(S)
+=======
+import package.egcdm2
+from package.egcdm2 import e_gcd
+def m1f1(NUM1, NUM2):
+    if NUM1 < NUM2:
+        NUM1, NUM2 = NUM2, NUM1
+    if NUM1 == NUM2 & NUM1 != 0 or NUM2 == 1:
+        S = str(NUM1) + ' * ' + str(1) + ' + ' + str(NUM2) + ' * ' + str(0) + ' = ' + str(0)
+    else:
+        if NUM2 == 0:
+            if NUM1 == 0:
+                S = str(NUM1) + ' * ' + str(0) + ' + ' + str(NUM2) + ' * ' + str(0) + ' = ' + str(0)
+            else:
+                S= str(NUM1) + ' * ' + str(1) + ' + ' + str(NUM2) + ' * ' + str(0) + ' = ' + str(0)
+        else:
+            S = str(NUM1) + ' * ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[0]) + ' + ' + str(NUM2) + ' * ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[1]) + ' = ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[2][2])
+    return(S)
+>>>>>>> 543d229... Module
--- package/egcdm2.py
+++ package/egcdm2.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import math
 
 
@@ -10,4 +11,18 @@ def e_gcd(first_num, sec_num):
     buf = kof2
     kof2 = kof1 - (math.ceil(first_num / sec_num) - 1) * kof2
     kof1 = buf
+=======
+import math
+
+
+def e_gcd(first_num, sec_num):
+    if sec_num == 0:
+        return first_num
+    kof2 = 1
+    kof1 = 0
+    gcd = e_gcd(sec_num, first_num % sec_num)
+    buf = kof2
+    kof2 = kof1 - (math.ceil(first_num / sec_num) - 1) * kof2
+    kof1 = buf
+>>>>>>> 543d229... Module
     return kof1, kof2, gcd
