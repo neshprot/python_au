e44b71a6a1863469440d91024f826b7c130f5905
neshprot <71090726+neshprot@users.noreply.github.com>
2021-05-25 00:02:46 +0300

    Rename egcdmod.py to egcd_mod.py

---
+++ package/egcd_mod.py
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+import egcdm1
+from package.egcdm1 import *
+print(package.egcdm1.m1f1(15912, 1404))
+
+
+=======
+import package.egcdm1
+from package.egcdm1 import *
+print(package.egcdm1.m1f1(15912, 1404))
+
+
+>>>>>>> 543d229... Module
