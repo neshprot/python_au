60dee3d9930da4eec5d8dd5698e995b45cdf8886
neshprot <71090726+neshprot@users.noreply.github.com>
2021-05-25 13:10:07 +0300

    Update egcd_mod.py

--- package/egcd_mod.py
+++ package/egcd_mod.py
@@ -3,11 +3,7 @@ import egcdm1
 from egcdm1 import *
 print(egcdm1.m1f1(15912, 1404))
 
-
-=======
 import egcdm1
 from egcdm1 import *
 print(egcdm1.m1f1(15912, 1404))
 
-
->>>>>>> 543d229... Module
