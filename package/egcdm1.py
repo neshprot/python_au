da4d800ac9cc260f129cbbbd2deacd63a1b33084
neshprot <71090726+neshprot@users.noreply.github.com>
2021-05-25 12:35:39 +0300

    Update egcdm1.py
    without package

--- package/egcdm1.py
+++ package/egcdm1.py
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
 import egcdm2
-from package.egcdm2 import e_gcd
+from egcdm2 import e_gcd
 def m1f1(NUM1, NUM2):
     if NUM1 < NUM2:
         NUM1, NUM2 = NUM2, NUM1
@@ -13,11 +13,11 @@ def m1f1(NUM1, NUM2):
             else:
                 S= str(NUM1) + ' * ' + str(1) + ' + ' + str(NUM2) + ' * ' + str(0) + ' = ' + str(0)
         else:
-            S = str(NUM1) + ' * ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[0]) + ' + ' + str(NUM2) + ' * ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[1]) + ' = ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[2][2])
+            S = str(NUM1) + ' * ' + str(egcdm2.e_gcd(NUM1, NUM2)[0]) + ' + ' + str(NUM2) + ' * ' + str(egcdm2.e_gcd(NUM1, NUM2)[1]) + ' = ' + str(egcdm2.e_gcd(NUM1, NUM2)[2][2])
     return(S)
 =======
-import package.egcdm2
-from package.egcdm2 import e_gcd
+import egcdm2
+from egcdm2 import e_gcd
 def m1f1(NUM1, NUM2):
     if NUM1 < NUM2:
         NUM1, NUM2 = NUM2, NUM1
@@ -30,6 +30,6 @@ def m1f1(NUM1, NUM2):
             else:
                 S= str(NUM1) + ' * ' + str(1) + ' + ' + str(NUM2) + ' * ' + str(0) + ' = ' + str(0)
         else:
-            S = str(NUM1) + ' * ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[0]) + ' + ' + str(NUM2) + ' * ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[1]) + ' = ' + str(package.egcdm2.e_gcd(NUM1, NUM2)[2][2])
+            S = str(NUM1) + ' * ' + str(egcdm2.e_gcd(NUM1, NUM2)[0]) + ' + ' + str(NUM2) + ' * ' + str(egcdm2.e_gcd(NUM1, NUM2)[1]) + ' = ' + str(egcdm2.e_gcd(NUM1, NUM2)[2][2])
     return(S)
 >>>>>>> 543d229... Module
